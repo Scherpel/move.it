d7e988c5bb7ef3c2ebf2fb9ccea6c02e044d552f
Lucas Scherpel <lucasguarnelli@hotmail.com>
2021-02-27 19:17:16 -0300

    Adding css module logic

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,11 +1,13 @@
+import styles from '../styles/components/ExperienceBar.module.css';
+
 export default function ExperienceBar() {
     return(
-        <header className="experience-bar">
+        <header className={styles.experienceBar}>
             <span>0xp</span>
             <div>
                 <div style={{width: '60%'}} />
-                <span className="current-experience" style={{left: '50%'}}>
-                    300pv
+                <span className={styles.currentExperience} style={{left: '50%'}}>
+                    300xp
                 </span>
             </div>
             <span>600xp</span>
