0809c92c12fa0ea532b827c669d96f472f982546
Lucas Scherpel <lucasguarnelli@hotmail.com>
2021-02-27 18:51:19 -0300

    Migrating project to Next and adding Typescript

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,14 @@
+export default function ExperienceBar() {
+    return(
+        <header className="experience-bar">
+            <span>0xp</span>
+            <div>
+                <div style={{width: '60%'}} />
+                <span className="current-experience" style={{left: '50%'}}>
+                    300pv
+                </span>
+            </div>
+            <span>600xp</span>
+        </header>
+    )
+}
